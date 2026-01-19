b418fa0f2ee91dbe596b672c7c832be9cb6f1f03
Hubert Zubik <hubertzubik@gmail.com>
2026-01-19 09:33:21 +0000

    Add reply route for messages

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -2,11 +2,22 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
+function stripHtml(html) {
+  return String(html)
+    .replace(/<br\s*\/?>/gi, "\n")
+    .replace(/<\/p>/gi, "\n\n")
+    .replace(/<[^>]+>/g, "")
+    .replace(/\n{3,}/g, "\n\n")
+    .trim();
+}
+
 export async function sendEmail({ to, subject, html }) {
   return resend.emails.send({
     from: "Olivia Weston <contact@oliviaweston.co.uk>",
     to,
     subject,
+    replyTo: "contact@oliviaweston.co.uk",
     html,
+    text: stripHtml(html),
   });
 }
